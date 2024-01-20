a3652208b3816ce203c405e95dc355fb26d6edae
Vivek Kashyap <humdumkashyap@gmail.com>
2024-01-20 23:39:47 +0530

    Remove Duplicates from Array

--- Day16.js
+++ Day16.js
@@ -1,4 +1,12 @@
 var removeDuplicates = function (nums) {
-  let data = new Set(nums);
-  condole.log;
+  let k = 1;
+
+  for (i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[k] = nums[i + 1];
+      k++;
+    }
+  }
+  console.log(k);
+  return k;
 };
