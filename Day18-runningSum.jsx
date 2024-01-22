4ff7435f001b145f17c5429993903a5a1678588d
Vivek Kashyap <humdumkashyap@gmail.com>
2024-01-22 22:32:42 +0530

    Running Sum

---
+++ Day18-runningSum.jsx
@@ -0,0 +1,40 @@
+// 🚀🚀🚀Day-18🚀🚀🚀
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+
+let arr = [1, 1, 1, 1, 1];
+
+function runningSum(arr) {
+  let newArr = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    newArr.push(sum);
+  }
+
+  return newArr;
+}
+
+console.log(runningSum(arr));
