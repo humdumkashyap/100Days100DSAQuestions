e24ba4d7e5f198e37a2af15c39834de9ff4779d4
Vivek Kashyap <humdumkashyap@gmail.com>
2024-01-22 22:34:52 +0530

    Majority Element

--- Day17-majorityElement.js
+++ Day17-majorityElement.js
@@ -1,3 +1,24 @@
+// 🚀🚀🚀Day-17🚀🚀🚀
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+
 // let arr=[2,2,1,1,2,2]
 
 let arr = [3, 2, 2, 2, 2, 2, 2, 2, 3];
@@ -17,7 +38,7 @@ function majorityElement(arr) {
 
   for (x in newArr) {
     if (newArr[x] > max) {
-        index = x;
+      index = x;
       max = newArr[x];
     }
   }
