7075e3b1af6a70fac15a4281d3a892d97ca5b5ec
Vivek Kashyap <humdumkashyap@gmail.com>
2024-02-27 00:09:21 +0530

    longest Palindrome

--- Day52-LongestPalindrome.js
+++ Day52-LongestPalindrome.js
@@ -1,3 +1,42 @@
-var longestPalindrome = function (s) {};
-     
+// Given a string s, return the longest
+// palindromic
 
+// substring
+//  in s.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+let arr = [];
+let substr;
+let substringvalue;
+
+for (let i = 0; i <= s.length; i++) {
+  for (let j = i + 1; j <= s.length; j++) {
+    substringvalue = s.substring(i, j);
+    if (!arr.includes(substringvalue)) {
+      arr.push(s.substring(i, j));
+    }
+  }
+}
+arr.sort((a, b) => b.length - a.length);
+console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+  substr = arr[i].split("").reverse().join("");
+  console.log(substr, "substr");
+  if (arr[i] == substr) {
+    return substr;
+  }
+}
