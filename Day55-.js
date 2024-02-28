d1a3d70abd45b9c29ff6c0d3bed64017cbd9bff5
Vivek Kashyap <humdumkashyap@gmail.com>
2024-02-28 23:14:43 +0530

    min Remove To Make Valid

---
+++ Day55-.js
@@ -0,0 +1,43 @@
+// 💪💪💪Day-55💪💪💪
+
+// Given a string s of '(' , ')' and lowercase English characters.
+
+// Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+// Formally, a parentheses string is valid if and only if:
+
+// It is the empty string, contains only lowercase characters, or
+// It can be written as AB (A concatenated with B), where A and B are valid strings, or
+// It can be written as (A), where A is a valid string.
+
+// Example 1:
+
+// Input: s = "lee(t(c)o)de)"
+// Output: "lee(t(c)o)de"
+// Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+// Example 2:
+
+// Input: s = "a)b(c)d"
+// Output: "ab(c)d"
+// Example 3:
+
+// Input: s = "))(("
+// Output: ""
+// Explanation: An empty string is also valid.
+
+// Link: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/description/
+
+function minRemoveToMakeValid(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    console.log(s[i]);
+    if (s[i] == "(") {
+      stack.push(")");
+    }
+  }
+
+  console.log(stack);
+}
+
+console.log(minRemoveToMakeValid("lee(t(c)o)de)"));
