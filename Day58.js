3110360f9b9f80a0639ac47e2fb0a0fa98ceb70b
Vivek Kashyap <humdumkashyap@gmail.com>
2024-03-03 00:03:13 +0530

    maximum Wealth

--- Day58.js
+++ Day58.js
@@ -1,7 +1,42 @@
 function maximumWealth(n) {
-  let maxMoney = Infinity;
-  n.reduce((acc, current) => (acc + current, 0));
+  //  sol-2
+  let maxMoney = -Infinity;
+  let sum;
+
+  function Reduce(singleSum) {
+    return singleSum.reduce((acc, current) => {
+      return acc + current;
+    }, 0);
+  }
+  for (let i = 0; i < n.length; i++) {
+    sum = Reduce(n[i]);
+    console.log(sum);
+    if (sum > maxMoney) {
+      maxMoney = sum;
+      console.log(maxMoney, "maxMoney");
+    }
+  }
+  return maxMoney;
+
+  //  sol- 1
+  // let maxMoney=-Infinity
+  // let sum
+
+  //  for( let i=0;i<n.length;i++){
+  //      sum= n[i].reduce((acc,current)=>{
+  //     return acc+current},0)
+  //   if(sum>maxMoney){
+  //       maxMoney=sum
+  //     console.log(maxMoney,"maxMoney")
+  //   }
+
+  // }
+  // return maxMoney
 }
-let n = [1, 2, 3];
-// let n=[[1,2,3],[3,2,1]]
-maximumWealth(n);
+// let n=[1,2,3]
+let n = [
+  [2, 8, 7],
+  [7, 1, 3],
+  [1, 9, 5],
+];
+console.log(maximumWealth(n));
