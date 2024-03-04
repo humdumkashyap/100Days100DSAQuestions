9ab1cf3d0efeee7599853b0283d506dd8340016e
Vivek Kashyap <humdumkashyap@gmail.com>
2024-03-04 23:43:56 +0530

    can Be Increasing

--- Day60-canBeIncreasing.js
+++ Day60-canBeIncreasing.js
@@ -1,11 +1,23 @@
 function canBeIncreasing(nums) {
-  let maxnum = Math.max.apply(0, nums);
-  console.log(nums.indexOf(maxnum));
-  if (nums.indexOf(maxnum) > nums.length - 1) {
-    return true;
-  } else {
-    return false;
+  for (let i = 0; i < nums.length; i++) {
+    let temp = [...nums];
+    temp.splice(i, 1);
+    if (isIncreasing(temp)) {
+      return true;
+    }
   }
+  return false;
 }
-let nums = [1, 2, 10, 5, 7];
-console.log(canBeIncreasing(nums));
+
+function isIncreasing(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(canBeIncreasing([1, 2, 10, 5, 7])); // Outputs: true
+console.log(canBeIncreasing([2, 3, 1, 2])); // Outputs: false
+console.log(canBeIncreasing([1, 1, 1])); // Outputs: false
