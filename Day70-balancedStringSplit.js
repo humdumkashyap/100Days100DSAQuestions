7449e49f8ead5afae6352c0f468110d0d0a2dd34
Vivek Kashyap <humdumkashyap@gmail.com>
2024-03-15 10:09:24 +0530

    balanced String Split

--- Day70-balancedStringSplit.js
+++ Day70-balancedStringSplit.js
@@ -1,1 +1,17 @@
-var balancedStringSplit = function (s) {};
+var balancedStringSplit = function (s) {
+  let count = 0;
+  let pairCount = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "R") {
+      count++;
+    } else {
+      if (s[i] == "L") {
+        count--;
+      }
+      if (count == 0) {
+        pairCount++;
+      }
+    }
+  }
+  return pairCount;
+};
