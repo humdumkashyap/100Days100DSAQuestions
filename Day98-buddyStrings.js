64e46921ba3077777b5dfff35c6af78a7ad4a5d4
Vivek Kashyap <humdumkashyap@gmail.com>
2024-04-13 23:09:25 +0530

    day 98 -buddy Strings

--- Day98-buddyStrings.js
+++ Day98-buddyStrings.js
@@ -1,1 +1,24 @@
-var buddyStrings = function (s, goal) {};
+var buddyStrings = function (s, goal) {
+  if (s.length !== goal.length) return false;
+  if (s === goal) {
+    const set = new Set(s);
+    return set.size < s.length;
+  }
+
+  let first = -1,
+    second = -1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== goal[i]) {
+      if (first === -1) first = i;
+      else if (second === -1) second = i;
+      else return false;
+    }
+  }
+
+  return (
+    second !== -1 && s[first] === goal[second] && s[second] === goal[first]
+  );
+};
+
+console.log(buddyStrings("ab", "ba")); //true
+console.log(buddyStrings("ab", "ab")); //false
