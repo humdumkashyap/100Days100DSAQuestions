674e633c455495259768fa738075316f597af468
Vivek Kashyap <humdumkashyap@gmail.com>
2024-04-19 23:08:04 +0530

    day 2 -is Anagram

--- Day2-ValidAnagram.js
+++ Day2-ValidAnagram.js
@@ -1,3 +1,20 @@
+// 🚀🚀🚀 Revision Day-2🚀🚀🚀
+
+//  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once .
+
+//  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Leetcode: https://leetcode.com/problems/valid-anagram/
+
 let s = "anagram";
 let t = "nagaram";
 var isAnagram = function (s, t) {
