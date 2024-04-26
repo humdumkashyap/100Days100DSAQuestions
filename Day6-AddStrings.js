a558ae35eb7a5ec1a8e3125a794937c54ee936f3
Vivek Kashyap <humdumkashyap@gmail.com>
2024-04-26 21:59:52 +0530

    day 6

--- Day6-AddStrings.js
+++ Day6-AddStrings.js
@@ -2,30 +2,31 @@
 
 // You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
 
- 
-
 // Example 1:
 
 // Input: num1 = "11", num2 = "123"
 // Output: "134"
 
 function addStrings(num1, num2) {
-    let res = [];
-    let i = num1.length - 1;
-    let j = num2.length - 1;
-    let carry = 0;
+  let res = [];
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let carry = 0;
 
-    while (i >= 0 || j >= 0) {
-        let sum = carry;
-        if (i >= 0) sum += num1[i--] - '0';
-        if (j >= 0) sum += num2[j--] - '0';
-        res.push(sum % 10);
-        carry = Math.floor(sum / 10);
-    }
+  while (i >= 0 || j >= 0) {
+    let sum = carry;
+    if (i >= 0) sum += num1[i--] - "0";
+    if (j >= 0) sum += num2[j--] - "0";
+    res.push(sum % 10);
+    carry = Math.floor(sum / 10);
+  }
 
-    if (carry) res.push(carry);
-    console.log(res.reverse().join(''))
-    return res.reverse().join('');
+  if (carry) res.push(carry);
+  console.log(res.reverse().join(""));
+  return res.reverse().join("");
 }
 
-addStrings("11", "123")
+console.log(addStrings("11", "123"));
+console.log(addStrings("456", "77"));
+console.log(addStrings("0", "0"));
+console.log(addStrings("1", "9"));
